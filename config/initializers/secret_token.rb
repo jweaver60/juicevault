a3d55b1ceeed12cd5950d35937ce6705b64bb27f
Jacob Weaver <jweaver60@gmail.com>
2014-02-19 18:13:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Juicevault::Application.config.secret_key_base = '37c835c3f183437827391a63e5ec3066e687b4e7d4e7687b3674c867988501a7e060c5d9b7aacbae05bee2b74873745f2e34f06279cd9082209590cca895798f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Juicevault::Application.config.session_store :cookie_store, key: '_juicevault_session'
