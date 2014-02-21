145e3684d7ab101ba842b75de9a2da5350473b46
Jacob Weaver <jweaver60@gmail.com>
2014-02-21 10:25:15 -0500

    fixed username stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_devise_params, if: :devise_controller?
+  
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:username, :email, :password, :password_confirmation)
+    end
+  end
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:username, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 
   def after_sign_in_path_for(resource)
   	juices_path
