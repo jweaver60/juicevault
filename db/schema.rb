7be4d1773a31ab72274f5aeef46720bdb172f3f1
Dustin Armstrong <dustin@dustinarmstrong.com>
2014-02-21 14:13:50 -0500

    Updated user page, changed topbar navigation, updated styles

--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,12 @@ ActiveRecord::Schema.define(version: 20140221181536) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "encrypted_password"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count"
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -47,5 +47,8 @@ ActiveRecord::Schema.define(version: 20140221181536) do
     t.datetime "updated_at"
     t.string   "username"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
