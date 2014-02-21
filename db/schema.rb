84ee0799113ba13005851025f9f527af547d16b9
Dustin Armstrong <dustin@dustinarmstrong.com>
2014-02-21 17:23:10 -0500

    Something

--- db/schema.rb
+++ db/schema.rb
@@ -67,13 +67,24 @@ ActiveRecord::Schema.define(version: 20140221215605) do
 
   add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
 
+  create_table "ratings", force: true do |t|
+    t.integer  "juice_id"
+    t.integer  "user_id"
+    t.integer  "score",      default: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ratings", ["juice_id"], name: "index_ratings_on_juice_id"
+  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id"
+
   create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "encrypted_password"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count"
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -82,5 +93,8 @@ ActiveRecord::Schema.define(version: 20140221215605) do
     t.datetime "updated_at"
     t.string   "username"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
