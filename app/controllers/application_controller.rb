20ed6290df6e61540146fdeb913753a74efe9eb2
Jacob Weaver <jweaver60@gmail.com>
2014-02-24 10:01:40 -0500

    fixed username issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_devise_params, if: :devise_controller?
-  
-  def configure_devise_params
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:username, :email, :password, :password_confirmation)
-    end
-  end
 
   def configure_devise_params
     devise_parameter_sanitizer.for(:account_update) do |u|
       u.permit(:username, :email, :password, :password_confirmation, :current_password)
+    end
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:username, :email, :password, :password_confirmation)
     end
   end
 
