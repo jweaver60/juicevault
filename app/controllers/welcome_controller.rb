4699cc6a47d01597a4a3a3a2a933c25fa223e253
Dustin Armstrong <dustin@dustinarmstrong.com>
2014-02-24 11:31:12 -0500

    Removed content from home page for restructure

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def index
-		@recent_users = User.find(:all, :limit => 10, :order => 'created_at DESC')
-		if params[:search]
-			@recent_juices = Juice.find(:all, :conditions => ['name LIKE ? OR manufacturer LIKE ? OR category LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%"])
-		else
-			@recent_juices = Juice.find(:all, :limit => 10, :order => 'created_at DESC')
-		end
+	
 	end
 	
 end
