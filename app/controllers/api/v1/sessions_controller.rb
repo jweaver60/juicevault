15604f9e8c9f1009f91fa11066a161fbbcfa123e
Jacob Weaver <jweaver60@gmail.com>
2014-03-11 09:16:21 -0400

    added API auth

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,29 @@
-class Api::V1::SessionsController < ApplicationController
-	skip_before_filter :verify_authenticity_token, :only => :create
+class Api::V1::SessionsController < Devise::SessionsController
+	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	respond_to :json
 
 	def create
-		respond_with User.all
+		warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+		render :status => 200,
+					 :json => { :success => true,
+						          :info => 'Logged in',
+											:data => { :auth_token => current_user.authentication_token } }
+	end
+
+	def destroy
+		warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+		current_user.update_column(:authentication_token, nil)
+		render :status => 200,
+					 :json => { :success => true,
+						          :info => 'Logged out',
+											:data => {} }
+	end
+
+	def failure
+		render :status => 401,
+					 :json => { :success => false,
+						          :info => 'Login failed',
+											:data => {} }
 	end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate_user_from_token!
   before_filter :configure_devise_params, if: :devise_controller?
 
   def configure_devise_params
@@ -16,5 +17,15 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
   	dashboard_index_path
 	end
+
+  private
+
+  def authenticate_user_from_token!
+    user_token = params[:user_token].presence
+    user = user_token && User.find_by_authentication_token(user_token.to_s)
+    if user
+      sign_in user, store: false
+    end
+  end
 
 end
