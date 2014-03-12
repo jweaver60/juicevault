6628d56caf2da557b465ee96ba8d4e79d89f5684
Jacob Weaver <jweaver60@gmail.com>
2014-03-12 18:27:23 -0400

    working now

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,15 +3,22 @@ class Api::V1::UsersController < ApplicationController
 	respond_to :json
 
 	def index
-		@user = User.find_by_authentication_token(params[:auth_token])
+		respond_with User.find_by_authentication_token(params[:auth_token])
 	end
 
 	private
 
 	def ensure_user_authenticated
-		@user = User.find_by_authentication_token(params[:auth_token])
-		if @user.authentication_token
-			@user
+		if params[:auth_token]
+			@user = User.find_by_authentication_token(params[:auth_token])
+			if @user == nil
+				render :status => 401,
+							 :json => { :success => false,
+							 						:info => 'Unauthorized',
+							 						:data => {} }
+			else
+				@user
+			end
 		else
 			render :status => 401,
 						 :json => { :success => false,
