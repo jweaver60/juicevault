d7d673a55cb764fcc462c9764b85847ed3d264aa
Jacob Weaver <jweaver60@gmail.com>
2014-03-12 17:54:55 -0400

    come on

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,14 +3,14 @@ class Api::V1::UsersController < ApplicationController
 	respond_to :json
 
 	def index
-		@user
+		@user = User.find_by_authentication_token(params[:auth_token])
 	end
 
 	private
 
 	def ensure_user_authenticated
 		@user = User.find_by_authentication_token(params[:auth_token])
-		if @user
+		if @user.authentication_token
 			@user
 		else
 			render :status => 401,
